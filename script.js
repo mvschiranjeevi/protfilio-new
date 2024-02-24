e9b590a51adf3c05007a8628ff91049be69bb609
Chiranjeevi Medam <mvschiranjeevi@gmail.com>
2024-02-23 17:39:03 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,154 @@
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-items");
+const btnToggleNav = document.querySelector(".menu-btn");
+const workEls = document.querySelectorAll(".work-box");
+const workImgs = document.querySelectorAll(".work-description");
+const mainEl = document.querySelector("main");
+const yearEl = document.querySelector(".footer-text span");
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to add animation class when section is in viewport
+function addAnimation() {
+  const section = document.getElementById("about");
+  const heading = section.querySelector("h2");
+  const paragraph = section.querySelector("p");
+
+  if (isInViewport(section)) {
+    heading.classList.add("animate");
+    paragraph.classList.add("animate");
+    window.removeEventListener("scroll", addAnimation); // Remove event listener once animations are triggered
+  }
+}
+
+// Event listener to trigger animation when scrolling
+window.addEventListener("scroll", addAnimation);
+
+const toggleNav = () => {
+  nav.classList.toggle("hidden");
+
+  // Prevent screen from scrolling when menu is opened
+  document.body.classList.toggle("lock-screen");
+
+  if (nav.classList.contains("hidden")) {
+    btnToggleNav.textContent = "menu";
+  } else {
+    // When menu is opened after transition change text respectively
+    setTimeout(() => {
+      btnToggleNav.textContent = "close";
+    }, 475);
+  }
+};
+
+btnToggleNav.addEventListener("click", toggleNav);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.localName === "a") {
+    toggleNav();
+  }
+});
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+    toggleNav();
+  }
+});
+
+// Animating work instances on scroll
+
+workImgs.forEach((workImg) => workImg.classList.add("transform"));
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    const [textbox, picture] = Array.from(entry.target.children);
+    if (entry.isIntersecting) {
+      picture.classList.remove("transform");
+      Array.from(textbox.children).forEach(
+        (el) => (el.style.animationPlayState = "running")
+      );
+    }
+  },
+  { threshold: 0.3 }
+);
+
+workEls.forEach((workEl) => {
+  observer.observe(workEl);
+});
+
+// Toggle theme and store user preferred theme for future
+
+const switchThemeEl = document.querySelector('input[type="checkbox"]');
+const storedTheme = localStorage.getItem("theme");
+
+switchThemeEl.checked = storedTheme === "dark" || storedTheme === null;
+
+switchThemeEl.addEventListener("click", () => {
+  const isChecked = switchThemeEl.checked;
+
+  if (!isChecked) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
+    switchThemeEl.checked = false;
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    localStorage.setItem("theme", "dark");
+  }
+});
+
+// Trap the tab when menu is opened
+
+const lastFocusedEl = document.querySelector('a[data-focused="last-focused"]');
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Tab" && document.activeElement === lastFocusedEl) {
+    e.preventDefault();
+    btnToggleNav.focus();
+  }
+});
+
+// Rotating logos animation
+
+const logosWrappers = document.querySelectorAll(".logo-group");
+
+const sleep = (number) => new Promise((res) => setTimeout(res, number));
+
+logosWrappers.forEach(async (logoWrapper, i) => {
+  const logos = Array.from(logoWrapper.children);
+  await sleep(1400 * i);
+  setInterval(() => {
+    let temp = logos[0];
+    logos[0] = logos[1];
+    logos[1] = logos[2];
+    logos[2] = temp;
+    logos[0].classList.add("hide", "to-top");
+    logos[1].classList.remove("hide", "to-top", "to-bottom");
+    logos[2].classList.add("hide", "to-bottom");
+  }, 5600);
+});
+
+yearEl.textContent = new Date().getFullYear();
+
+var typed = new Typed(".typing-text", {
+  strings: [
+    "Full Stack Development",
+    "DevOps Development",
+    "System Engineering",
+    "Software Testing",
+  ],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 500,
+});
